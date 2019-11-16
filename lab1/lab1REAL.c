d1962674b71b1718659df92718421ab21684f17e
PontusEdvardsson <Pontuscvedvardsson@gmail.com>
2019-11-16 10:19:52 +0100

    started mathfunctions, not sorking atm

--- lab1/lab1REAL.c
+++ lab1/lab1REAL.c
@@ -2,29 +2,58 @@
 #include <string.h>
 
 void removeWhiteSpace(char *input);
-;
 
 int main()
 {
 	system("cls");
 	char ssn1[10]; //social security number (size 10 yymmddxxxx)
   char ssn2[10]; //social security number (size 10 yymmddxxxx)
-  int handler = 1;
+  int handler1 = 1;
+  int handler2 = 0;
+  int handler3 = 0;
 
-  while(handler == 1)
+  while(handler1 == 1)
   {
+    int totctrl = 0;
     printf("Välkommen! Ange ett personnummer:\n");
     fgets(ssn1, 10, stdin);
     removeWhiteSpace(ssn1);
-    printf("\nssn1 is now:%s", &ssn1);
+    printf("\nssn1 is now:%s\n\n", &ssn1);
 
     printf("Ange ett andra personnummer:\n");
     fgets(ssn2, 10, stdin);
     removeWhiteSpace(ssn2);
-    printf("\nssn2 is now:%s", &ssn2);
+    printf("\nssn2 is now:%s\n\n", &ssn2); 
+
+    for (int i = 0; i < 10; i++)
+    {
+      int temp = 0;
+      temp = ssn1[i-1];
+      switch (handler3)
+​      {
+        case 0:
+          temp = temp*2;
+          handler3 = 1;
+          break;
+        case 1:
+          handler3 = 0
+          break;
+      }
+      
+      if(temp>10)
+      {
+        char chartemp[2] = temp;
+        temp = chartemp[0] + chartemp[1];
+      }
+      totctrl += temp;
+    }
+    
+
+
+
 
 		printf("\n\nRun again? 1 = yes. 2 = no\n"); //no errorhandling yet
-		scanf("%d", &handler);
+		scanf("%d", &handler1);
     getchar();
   }
 
