234867f4da1c194b0b02fd8cb0da865fecbe4892
PontusEdvardsson <Pontuscvedvardsson@gmail.com>
2019-11-21 10:24:10 +0100

    fixed  input

--- lab1/lab1REAL.c
+++ lab1/lab1REAL.c
@@ -6,38 +6,41 @@ void removeWhiteSpace(char *input);
 int main()
 {
 	system("cls");
-	char ssn1[10]; //social security number (size 10 yymmddxxxx)
-  char ssn2[10]; //social security number (size 10 yymmddxxxx)
+	char ssn1[25]; //social security number (size 10 yymmddxxxx)
+  char ssn2[25]; //social security number (size 10 yymmddxxxx)
   int handler1 = 1;
   int handler2 = 0;
   int handler3 = 0;
   char chartemp[2];
+  int temp = 0;
 
   while(handler1 == 1)
   {
     int totctrl = 0;
     printf("Välkommen! Ange ett personnummer:\n");
-    fgets(ssn1, 10, stdin);
+    fgets(ssn1, 12, stdin);
     removeWhiteSpace(ssn1);
     printf("\nssn1 is now:%s\n\n", &ssn1);
 
     printf("Ange ett andra personnummer:\n");
-    fgets(ssn2, 10, stdin);
+    fgets(ssn2, 12, stdin);
     removeWhiteSpace(ssn2);
     printf("\nssn2 is now:%s\n\n", &ssn2); 
 
     for (int i = 0; i < 10; i++)
     {
-      int temp = 0;
+      printf("for1-");
       temp = ssn1[i-1];
 
       switch (handler3)
       {
       case 0:
+      printf("case0-");
         temp = temp*2;
-        handler3 = 0;
+        handler3 = 1;
         break;
       case 1:
+      printf("case1-");
         handler3 = 0;
         break;
       default:
@@ -48,9 +51,12 @@ int main()
       {
         chartemp[2] = temp;
         temp = chartemp[0] + chartemp[1];
+        printf("crtlover10-");
       }
       totctrl += temp;
+      printf("totctrl: %d \n", &totctrl);
     }
+    printf("Kontrollsiffra: %d", &totctrl);
     
 
 
